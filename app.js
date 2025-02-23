366e5e7084835fa0b824658a56e58af7da69af4a
bistik <erickbianes@gmail.com>
2025-02-23 17:54:13 +0800

    Submission for exercise4.1 and exercise4.2

---
+++ app.js
@@ -0,0 +1,28 @@
+const config = require('./utils/config')
+const express = require('express')
+const cors = require('cors')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const mongoose = require('mongoose')
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(middleware.requestLogger)
+app.use('/api/blogs', blogsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
